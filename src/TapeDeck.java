83210742e47bafe717e15a8f1e80fe1976afb809
badnew <badnew@mail.ru>
2016-07-01 23:41:37 +0300

    Head First стр.72 (ex.A)

---
+++ src/TapeDeck.java
@@ -0,0 +1,11 @@
+public class TapeDeck {
+    boolean canRecord = false;
+
+    void playTape() {
+        System.out.println("Пленка проигрывается");
+    }
+
+    void recordTape() {
+        System.out.println("Идет запись на пленку");
+    }
+}
